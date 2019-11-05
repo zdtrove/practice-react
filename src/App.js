fc3669c86d215973861406eb77e078d55811b047
Your Name <you@example.com>
2019-11-06 00:59:57 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div className="App">
+            <p>Hello</p>
+        </div>
+    );
+}
+
+export default App;
