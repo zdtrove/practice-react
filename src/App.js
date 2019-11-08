ed96d9d32e7c126d50ef22041f49d97185b552d2
Hoang An <tonnguyenhoangan@gmail.com>
2019-11-08 13:53:44 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 
+const Temp = (props) => {
+    return (
+        <React.Fragment>
+            {props.greetings}
+        </React.Fragment>
+    );
+}
+
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-                React App
-        </div>
+                <Temp greetings="hi" />
+            </div>
         );
     };
 }
