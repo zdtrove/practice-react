510b238aa31b65f1330f19c6cdb9d89f867e53af
Hoang An <tonnguyenhoangan@gmail.com>
2019-11-08 10:39:20 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        React App
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        name: 'myname'
+    }
+
+    changeName = (newName) => {
+        this.setState({
+            name: newName
+        });
+    }
+
+    changeNameFromInput = (event) => {
+        this.setState({
+            name: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <button onClick={() => this.changeName('new Name')}>Change state using Anon Function</button>
+                <button onClick={this.changeName.bind(this, 'new Name 2')}>Change state using bind Function</button>
+                <hr />
+                <input type="text" onChange={this.changeNameFromInput} value={this.state.name} />
+                <hr />
+                <div>{this.state.name}</div>
+            </div>
+        );
+    }
 }
 
 export default App;
