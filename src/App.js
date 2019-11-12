edfed98112faacc708dc40d603d4ce06de4c50e8
Hoang An <tonnguyenhoangan@gmail.com>
2019-11-12 16:36:54 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
 class App extends React.Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: 'new title 1',
+                completed: false
+            },
+            {
+                id: 2,
+                title: 'new title 2',
+                completed: false
+            },
+            {
+                id: 3,
+                title: 'new title 3',
+                completed: false
+            }
+        ]
+    }
     render() {
         return (
             <div className="App">
-                React App
+                <Todos todos={this.state.todos} />
             </div>
         );
     };
---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Todos extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Todos</h1>
+            </div>
+        );
+    };
+}
+
+export default Todos;
