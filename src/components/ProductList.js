aac278a57a837a2e372232ac6e65733272d1a31b
Your Name <you@example.com>
2019-12-02 00:06:08 +0700

    cm

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import Product from './Product';
 import Title from './Title';
+import { ProductConsumer } from '../context';
 
 export default class ProductList extends Component {
-	state = {
-		products: []
-	}
-
     render() {
         return <>
             <div className="py-5">
             	<div className="container">
+                    <Title name="our" title="products" />
             		<div className="row">
-            			<Title name="our" title="products" />
-            		</div>
+                        <ProductConsumer>
+                            {(value) => {
+                                return value.products.map(product => {
+                                    return <Product key={product.id} product={product} />
+                                });
+                            }}
+                        </ProductConsumer>
+                    </div>
             	</div>
             </div>
         </>
