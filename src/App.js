b7abd2ac00b22b6da230faeb207be80036133693
Your Name <you@example.com>
2019-12-12 07:23:03 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import HookCounterFour from './components/hook04/HookCounterFour';
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
+            	<HookCounterFour />
             </div>
         );
     };
--- src/components/hook03/HookCounterThree.js
+++ src/components/hook03/HookCounterThree.js
@@ -7,15 +7,16 @@ function HookCounterThree() {
             <input 
                 type="text" 
                 value={name.firstName} 
-                onChange={e => setName({ firstName: e.target.value })} 
+                onChange={e => setName({ ...name, firstName: e.target.value })} 
             />
             <input 
                 type="text"
                 value={name.lastName}
-                onChange={e => setName({ lastName: e.target.value })}
+                onChange={e => setName({ ...name, lastName: e.target.value })}
             />
             <h2>Your First Name is - {name.firstName}</h2>
             <h2>Your Last Name is - {name.lastName}</h2>
+            <h2>{JSON.stringify(name)}</h2>
         </form>
     );
 }
