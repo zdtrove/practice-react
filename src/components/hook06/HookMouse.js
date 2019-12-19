74fcd19dae848896a45dde687d569ed87515418e
Hoang An <antnh@icd-vn.com>
2019-12-19 08:34:12 +0700

    Commit test git

--- src/components/hook06/HookMouse.js
+++ src/components/hook06/HookMouse.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react'
 function HookMouse() {
     const [x, setX] = useState(0);
     const [y, setY] = useState(0);
-
+    // Test git
     const logMousePosition = e => {
         console.log('Mouse event');
         setX(e.clientX);
