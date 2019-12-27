4f4edcf1edf05eb3a7e572a29d68672c2502bbdb
Your Name <you@example.com>
2019-12-27 22:01:21 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
-import CakeContainer from './components/CakeContainer';
-import HooksCakeContainer from './components/HooksCakeContainer';
-import IceCreamContainer from './components/IceCreamContainer';
+// import CakeContainer from './components/CakeContainer';
+// import NewCakeContainer from './components/NewCakeContainer';
+// import HooksCakeContainer from './components/HooksCakeContainer';
+// import IceCreamContainer from './components/IceCreamContainer';
+// import ItemContainer from './components/ItemContainer';
+import UserContainer from './components/UserContainer';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
@@ -11,9 +14,15 @@ class App extends React.Component {
         return (
         	<Provider store={store}>
 	            <div className="App">
+	            	<UserContainer />
+	        	{/* 
+					<ItemContainer cake />
+	            	<ItemContainer />
 	                <CakeContainer />
 	                <HooksCakeContainer />
 	                <IceCreamContainer />
+	                <NewCakeContainer />
+	        	*/}
 	            </div>
 	        </Provider>
         );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+const store = createStore(
+	rootReducer, 
+	composeWithDevTools(applyMiddleware(logger, thunk)),
+);
 
 export default store;
