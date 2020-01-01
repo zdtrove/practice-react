b4deb100322c7a5b1ace6e96ff5f6354fb1dd48a
Your Name <you@example.com>
2020-01-01 21:03:39 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { SportsStoreDataStore } from "./data/DataStore";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { ShopConnector } from "./shop/ShopConnector";
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                React App
-            </div>
-        );
-    };
+export default class App extends Component {
+	render() {
+		return <Provider store={ SportsStoreDataStore }>
+			<Router>
+				<Switch>
+					<Route path="/shop" component={ ShopConnector } />
+					<Redirect to="/shop" />
+				</Switch>
+			</Router>
+		</Provider>
+	} 
 }
-
-export default App;
