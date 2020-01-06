2d806b59bbb56fdcf83dafe378f7195c174a97ce
Hoang An <antnh@icd-vn.com>
2020-01-06 16:56:46 +0700

    cm

---
+++ src/Message.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Message(props) {
+    return <h4 className="bg-success text-white text-center p-2">
+        {props.greeting}, {props.name}
+    </h4>
+}
---
+++ src/SimpleButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function SimpleButton(props) {
+    return (
+        <button onClick={props.callback} className={props.className}>
+            {props.text}
+        </button>
+    )
+}
