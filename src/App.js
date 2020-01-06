2a6b69c5c541480f79043f015ed6b65ce618efd7
Your Name <you@example.com>
2020-01-06 23:40:28 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                React App
-            </div>
-        );
-    };
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			message: "Ready"
+		}
+	}
+
+	handleEvent = (event) => {
+		if (event.type === "mousedown") {
+			this.setState({ message: "Down"});
+		} else {
+			this.setState({ message: "Up"});
+		}
+	}
+
+	render() {
+		return (
+			<div className="m-2">
+				<div className="h4 bg-primary text-white text-center p-2">
+					{ this.state.message }
+				</div>
+				<div className="text-center">
+					<button 
+						className="btn btn-primary" 
+						onMouseDown={ this.handleEvent }
+						onMouseUp={ this.handleEvent }
+					>Click Me</button>
+				</div>
+			</div>
+		) 
+	} 
 }
-
-export default App;
