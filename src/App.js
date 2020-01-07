0ae3203f5a5e48af4393e5303057d2bc505df32a
Your Name <you@example.com>
2020-01-08 00:40:08 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import React, { Component } from 'react';
+import { ThemeButton } from "./ThemeButton";
 
 export default class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			message: "Ready"
+			message: "Ready",
+			counter: 0,
+			theme: "secondary"
 		}
 	}
 
-	handleEvent = (event) => {
-		if (event.type === "mousedown") {
-			this.setState({ message: "Down"});
-		} else {
-			this.setState({ message: "Up"});
-		}
+	selectTheme = (newTheme) => {
+		this.setState({
+			theme: newTheme,
+			message: `Theme: ${newTheme}`
+		});
 	}
 
 	render() {
 		return (
 			<div className="m-2">
-				<div className="h4 bg-primary text-white text-center p-2">
+				<div className={ `h4 bg-${this.state.theme} text-white text-center p-2` }>
 					{ this.state.message }
 				</div>
 				<div className="text-center">
-					<button 
-						className="btn btn-primary" 
-						onMouseDown={ this.handleEvent }
-						onMouseUp={ this.handleEvent }
-					>Click Me</button>
+					<ThemeButton theme="primary" callback={ this.selectTheme } />
+					<ThemeButton theme="danger" callback={ this.selectTheme } />
 				</div>
 			</div>
 		) 
