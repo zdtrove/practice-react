024e90f6cc40265b0e97041e74e523cc943caafc
Your Name <you@example.com>
2020-01-08 23:49:31 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -18,17 +18,23 @@ export default class App extends Component {
 		});
 	}
 
+	handleClick= (event) => {
+		console.log(`App: Type: ${event.type} `
+		+ `Target: ${event.target.tagName} `
+		+ `CurrentTarget: ${event.currentTarget.tagName}`);
+	}
+
 	render() {
 		return (
-			<div className="m-2">
+			<header className="m-2" onClick={ this.handleClick }>
 				<div className={ `h4 bg-${this.state.theme} text-white text-center p-2` }>
 					{ this.state.message }
 				</div>
-				<div className="text-center">
+				<div className="text-center" onClick={ this.handleClick }>
 					<ThemeButton theme="primary" callback={ this.selectTheme } />
 					<ThemeButton theme="danger" callback={ this.selectTheme } />
 				</div>
-			</div>
+			</header>
 		) 
 	} 
 }
--- src/ThemeButton.js
+++ src/ThemeButton.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 
 export class ThemeButton extends Component {
-	handleClick = (event) => {
+	handleClick = (event, capturePhase = false) => {
 		console.log(`ThemeButton: Type: ${event.type} `
 		+ `Target: ${event.target.tagName} `
 		+ `CurrentTarget: ${event.currentTarget.tagName}`);
-		this.props.callback(this.props.theme);
+		if (capturePhase) {
+			if (this.props.theme === "danger") {
+				event.stopPropagation();
+				console.log("Stopped event");
+			} else {
+				console.log("Skipped function prop: capture phase");
+			}
+		} else if (event.bubbles && event.currentTarget !== event.target) {
+			console.log("Skipped function prop: bubble phase");
+		} else {
+			console.log("Invoked function prop");
+			this.props.callback(this.props.theme);
+		}
 	}
 
 	render() {
-		return <span className="m-1" onClick={ this.handleClick }>
+		return <span className="m-1" onClick={ this.handleClick } onClickCapture={ (e) => this.handleClick(e, true) }>
 			<button 
 				className={`btn btn-${this.props.theme}`}
 				onClick={ this.handleClick }
