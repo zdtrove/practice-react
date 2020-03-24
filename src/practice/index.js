549f749c9fb4d9753310556745a9d18c98cc29a8
Your Name <you@example.com>
2020-03-24 21:11:53 +0700

    cm

--- src/practice/index.js
+++ src/practice/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 // import HotelReservation from './hotel-reservation';
 // import PhoneEcommerce from './phone-ecommerce';
 // import ComputerEcommerce from './computer-ecommerce';
-import TechStore from './tech-store';
+// import TechStore from './tech-store';
 
 export default class Main extends Component {
     render() {
